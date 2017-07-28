51338a4ccefbadd59e89812ba8a01c6e0a3b14c2
Wilson Yu <hello@wilsonyu.io>
2017-07-28 00:47:23 -0400

    implement front end components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,60 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './toolbox/theme.css';
+import theme from './toolbox/theme.js'
+import ThemeProvider from 'react-toolbox/lib/ThemeProvider';
+import Button from 'react-toolbox/lib/button/Button';
+import Card from 'react-toolbox/lib/card/Card';
+import CardActions from 'react-toolbox/lib/card/CardActions';
+import CardTitle from 'react-toolbox/lib/card/CardTitle';
+import Input from 'react-toolbox/lib/input/Input';
+import DatePicker from 'react-toolbox/lib/date_picker/DatePicker';
+
+
+const datetime = new Date(2015, 10, 16);
+const min_datetime = new Date(new Date(datetime).setDate(8));
+datetime.setHours(17);
+datetime.setMinutes(28);
 
 class App extends Component {
+  state = {
+    name: '',
+    message: '',
+    expDate: ''
+  };
+
+  handleChange = (name, value) => {
+    this.setState({...this.state, [name]: value});
+  };
+
+  encrypt = () => {
+    console.log(this.state);
+  }
+
+  decrypt = () => {
+    console.log(this.state);
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div style={{position: 'absolute', left: '50%', top: '50%', transform: 'translate(-50%, -50%)'}}>
+        <ThemeProvider theme={theme}>
+            <Card style={{width: '375px', height: '400px'}}>
+              <CardTitle title="Tovia's Engima" />
+              <CardTitle avatar='https://ssl.gstatic.com/images/branding/product/1x/avatar_circle_grey_512dp.png'>
+                <Input type='text' label='Name' name='name' value={this.state.name} onChange={this.handleChange.bind(this, 'name')}  />
+              </CardTitle>
+              <Input type='text' multiline label='Message' maxLength={120} value={this.state.message} onChange={this.handleChange.bind(this, 'message')} />
+              <DatePicker label='Expiration date' sundayFirstDayOfWeek minDate={min_datetime} onChange={this.handleChange.bind(this, 'expDate')} value={this.state.expDate} />
+              <CardActions>
+                <Button label='Encrypt' onClick={this.encrypt.bind(this)} />
+                <Button label='Decrypt' onClick={this.decrypt.bind(this)} />
+              </CardActions>
+            </Card>
+        </ThemeProvider>
+        <div style={{'text-align': 'center'}}>
+          <p> Your Passphrase - STUFF </p>
+          <p> Generate new Passphrase </p>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
